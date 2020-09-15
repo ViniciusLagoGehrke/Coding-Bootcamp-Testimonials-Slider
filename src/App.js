c42689fcb2af87d6a3da980bc79145a3c22ca011
Vinicius Lago Gehrke <vinictus@gmail.com>
2020-09-15 21:35:58 +0000

    Finished mobile layout

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import React from 'react';
+import './reset.css';
 import './App.css';
+import tanya from './img/image-tanya.jpg';
+import john from './img/image-john.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <body>
-        <div>
-          <p>
-          “ I’ve been interested in coding for a while but never taken the jump, until now. 
-          I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
-          excited about the future. ”
-          </p>
-          <div>
+    <>
+      <div className="body">
+        <div className="card">
+          <div className="hero">
+            <img className="img" src={tanya} alt="tanya perfil" />
+          </div>
+          <div className="main">
+            <p>“ I’ve been interested in coding for a while but never taken the jump, until now. 
+            I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
+            excited about the future. ”</p>
             <h6><strong>Tanya Sinclair</strong></h6>
             <h6>UX Engineer</h6>
           </div>
         </div>
-        <div>
-          <p>“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”
-          </p>
-          <div>
+        <div className="card">
+          <div className="hero">
+            <img className="img" src={john} alt="john perfil" />
+          </div>
+          <div className="main">
+            <p>“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”</p>
             <h6><strong>John Tarkpor</strong></h6>
             <h6>Junior Front-end Developer</h6>
           </div>
-        </div>
-        <div class="attribution">
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-          Coded by <a href="#">Your Name Here</a>.
-        </div>
-      </body>
-    </div>
+        </div>      
+      </div>
+      <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+          Coded by <a href="https://github.com/ViniciusLagoGehrke">Vinicius Gehrke</a>.
+      </div>
+    </>
   );
 }
 
