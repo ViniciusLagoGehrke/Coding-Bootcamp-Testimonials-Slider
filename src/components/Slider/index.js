86e6ad35b844878e4cc338410ca0a78445b13f73
Vinicius Lago Gehrke <vinictus@gmail.com>
2020-09-18 09:47:07 +0000

    slider Layout and animation

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './index.css';
 
-export default function Slider() {
-  return (
-    <>
+class Slider extends React.Component {
+  
+  saySomething(something){
+    console.log(something);
+  }
+  
+  handleClick(e) {
+    this.saySomething(this);
+  }
+
+  render() {
+    return <div className="body">
       <div className="slider">
-        <button type="button" className="btn left">
-        </button>
-        <button type="button" className="btn right">
-        </button>
+          <button onClick={this.handleClick.bind(this)} type="button" className="btn left">
+          </button>
+          <button onClick={this.handleClick.bind(this)} type="button" className="btn right">
+          </button>
       </div>
-    </>
-  );
+     </div>;
+  }
 }
+
+export default Slider;
